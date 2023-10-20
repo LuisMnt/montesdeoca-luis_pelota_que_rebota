dbcdbebd556633877585888ef08dd63209dc9c0f
LuisMnt <djlgkp501@gmail.com>
2023-10-20 13:14:01 -0500

    backG rand, diam random, color pelota rand al rebotar

--- sketch.js
+++ sketch.js
@@ -1,45 +1,57 @@
-let posX, posY, diam, rad;
-
-let espe;
-let margen;
-let vel;
-let piso;
-let acel;
+let ball;
+let gravity = 0.99;
+let friction = 2;
+let backgroundColor;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  posX = windowWidth / 2;
-  posY = 0;
-  rectMode(CENTER);
-  diam = random(10, 50);
-  rad = diam / 2;
-  espe = 50;
-  margen = 40;
-  piso = windowHeight - margen - espe / 2;
-  vel = 0;
-  acel = 0.98;
+  ball = new Ball(width / 2, height / 2, random(1, 40));
+  backgroundColor = color(random(20, 255), random(50, 100), random(100, 200));
 }
 
 function draw() {
-  background(255, 150, 50);
+  background(backgroundColor);
 
-  actualizar();
-
-  noStroke();
-  fill("white");
-  circle(posX, posY, 20);
-
-  fill("red");
-  rect(windowWidth / 2, windowHeight - margen, windowWidth, espe);
+  ball.update();
+  ball.display();
 }
 
-function actualizar() {
-  vel += acel;
-  posY += vel;
+class Ball {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+    this.color = color(random(10, 255), random(0, 200), random(0, 200));
+  }
 
-  if (posY > piso - rad) {
-    print("ya");
-    vel *= -1;
-    posY += vel;
+  update() {
+    this.ySpeed += gravity;
+    this.xSpeed *= friction;
+    this.y += this.ySpeed;
+    this.x += this.xSpeed;
+
+    if (this.y > height - this.radius / 2) {
+      this.radius = random(10, 200) / 2;
+      this.color = color(random(10, 255), random(0, 200), random(0, 200));
+      backgroundColor = color(
+        random(20, 255),
+        random(50, 100),
+        random(100, 200)
+      );
+      background(backgroundColor);
+      this.y = height - this.radius / 2;
+      this.ySpeed *= -1;
+    }
+
+    if (this.x > width - this.radius / 2 || this.x < this.radius / 2) {
+      this.xSpeed *= -1;
+    }
+  }
+
+  display() {
+    fill(this.color);
+    ellipse(this.x, this.y, this.radius * 2);
   }
 }
